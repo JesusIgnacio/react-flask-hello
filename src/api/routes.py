92b3c147af7fcaa33f446beb99b4c1c22c3d75ee
JCastillo <ignacio.casbar@gmail.com>
2023-01-15 17:17:38 +0000

    update login

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,14 +1,18 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, make_response
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+import uuid
+from  werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required
 
 api = Blueprint('api', __name__)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
+@jwt_required()
 def handle_hello():
 
     response_body = {
@@ -16,3 +20,70 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods =['POST'])
+def signup():
+    # creates a dictionary of the form data
+    data = request.form
+
+    # gets name, email and password
+    email = data.get('email')
+    password = data.get('password')
+
+    # checking for existing user
+    user = User.query\
+        .filter_by(email = email)\
+        .first()
+
+    if not user:
+        # database ORM object
+        user = User(
+            id = str(uuid.uuid4()),
+            email = email,
+            password = generate_password_hash(password),
+            is_active = True
+        )
+        # insert user
+        db.session.add(user)
+        db.session.commit()
+
+        return make_response('Successfully registered.', 201)
+    else:
+        # returns 202 if user already exists
+        return make_response('User already exists. Please Log in.', 202)
+
+@api.route('/login', methods =['POST'])
+def login():
+    # creates dictionary of form data
+    auth = request.form
+
+    if not auth or not auth.get('email') or not auth.get('password'):
+        # returns 401 if any email or / and password is missing
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate' : 'Basic realm ="Login required !!"'}
+            )
+
+    user = User.query\
+        .filter_by(email = auth.get('email'))\
+        .first()
+
+    if not user:
+        # returns 401 if user does not exist
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate' : 'Basic realm ="Wrong User or Password !!"'}
+        )
+
+    if check_password_hash(user.password, auth.get('password')):
+        # generates the JWT Token
+        access_token = create_access_token(identity=user.id)
+        return jsonify({ "token": access_token, "user_id": user.id })
+    # returns 403 if password is wrong
+    return make_response(
+        'Could not verify',
+        403,
+        {'WWW-Authenticate' : 'Basic realm ="Wrong User or Password !!"'}
+    )
